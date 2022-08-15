8b23af5f2bc84dcaeb623802c4dd9516a2df3f89
Beshoy Ayad <beshoy.ayad@spinneys-egypt.com>
2022-08-16 01:47:58 +0200

    fixed lint errors

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-9wjw3whe.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.eba-9wjw3whe.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
